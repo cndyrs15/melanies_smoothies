cffb4eedb07843af37dca48ebe67221bd5e9ccad
cndyrs15 <cindy.rose.santos@accenture.com>
2024-11-19 10:21:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,8 @@ ingredients_list = st.multiselect(
     , max_selections=5
     )
 
-if(ingredients_list):
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+if ingredients_list:
+    ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
@@ -39,11 +38,11 @@ if(ingredients_list):
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-    values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
 
 time_to_insert = st.button('Submit Order')
